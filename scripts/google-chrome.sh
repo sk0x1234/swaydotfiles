2a6b54d1b25c5c9b1b45cae63f87011f747441d9
Naruto Windy <narutowindy@users.noreply.github.com>
2017-08-20 18:16:12 +0530

    Add files via upload

--- scripts/google-chrome.sh
+++ scripts/google-chrome.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-chromium -kiosk https://encrypted.google.com/ 2>/dev/null & 
+
+chromium \
+	--disable \
+	--disable-translate \
+	--disable-infobars \
+	--disable-suggestions-service \
+	--start-maximized \
+	--kiosk https://encrypted.google.com/ 2>/dev/null & 
 disown
 
