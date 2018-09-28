a8029a576438b278a2afd90b5c028f28d00a81e2
narutowindy <narutowindy@users.noreply.github.com>
2018-09-27 23:15:17 -0400

    Add files via upload

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,6 @@
-# Globally recognised variables
-set -g -x VISUAL nvim
-set -g -x EDITOR nvim
-set -g -x PAGER less 
-
+# Start X at login
+if status is-login
+    if test -z "$DISPLAY" -a $XDG_VTNR = 1
+        exec startx -- -keeptty
+    end
+end
