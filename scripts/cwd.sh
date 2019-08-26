cda8ab77113482a36ea4ff86d18e805913780b9e
Srinath koilakonda <srinath.koilakonda@gmail.com>
2019-08-25 21:55:38 -0400

    Update cwd.sh

--- scripts/cwd.sh
+++ scripts/cwd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 hostname=`hostname`
-export PWD=`i3-msg -t get_tree | sed 's/{/{\n/g' | grep -A 5 "\"focused\":true" | sed 's/,/\n/g' | grep $hostname | sed 's/\"//g' | awk 'BEGIN {FS=":"};{print $3}' | sed 's/\~/\/root/g' | head -1` 
+export PWD=`sway-msg -t get_tree | sed 's/{/{\n/g' | grep -A 5 "\"focused\":true" | sed 's/,/\n/g' | grep $hostname | sed 's/\"//g' | awk 'BEGIN {FS=":"};{print $3}' | sed 's/\~/\/root/g' | head -1` 
 
 cd "$PWD"
 exec urxvt &
